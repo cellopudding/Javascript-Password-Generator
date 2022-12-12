e59abf341d0528742398a510701c89f3092e229a
Lonnie Root <root.lonnie@gmail.com>
2022-12-12 12:47:59 -0500

    saving changes

--- Assets/style.js
+++ Assets/style.js
@@ -1,7 +1,11 @@
-var Lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var Uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var Numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var Symbols = ["!", "@", "#", "$", "%", "^", "&", "*"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numbers = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*"]
+
+var result = []
+var possibleCharacters = []
+var guaranteedCharacters = []
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function getRandom(arr){
@@ -14,14 +18,14 @@ function generatePassword() {
     
     //1. Prompt user for the password criteria 
 //   a. Password length between 8-128 (prompt)
-var L = parseInt(prompt ("How many characters would you like password to contain between 8-128"))
-
-if (L< 8 || L > 128) {
+var len = parseInt(prompt ("How many characters would you like password to contain between 8-128"))
+console.log(len)
+if (len< 8 || len > 128) {
     alert ("Must be between 8-128 characters")
     return null
 }
 
-if (Number.isNaN(L)) {
+if (Number.isNaN(len)) {
     alert ("Must be a number")
     return null
 }
@@ -44,50 +48,57 @@ if (!hasLowercase && !hasUppercase && !hasNumbers && !hasSymbols) {
 //Save Criteria to an object
 
 var passwordCriteria = {
-    L:L,
+    len:len,
     hasLowercase:hasLowercase,
     hasUppercase:hasUppercase,
     hasNumbers:hasNumbers,
     hasSymbols:hasSymbols,
 }
 
+
 //3. Generate password based on criteria
-var result = []
-var possibleCharacters = []
-var guaranteedCharacters = []
+
 
 if (!passwordCriteria) {
     return null
 }
 if (passwordCriteria.hasLowercase) {
-    possibleCharacters = possibleCharacters.concat(Lowercase)
-    guaranteedCharacters.push(getRandom(Lowercase))
+    possibleCharacters = possibleCharacters.concat(lowercase)
+    guaranteedCharacters.push(getRandom(lowercase))
 }
+
 if (passwordCriteria.hasUppercase) {
-    possibleCharacters = possibleCharacters.concat(Uppercase)
-    guaranteedCharacters.push(getRandom(Uppercase))
+    possibleCharacters = possibleCharacters.concat(uppercase)
+    guaranteedCharacters.push(getRandom(uppercase))
 }
 if (passwordCriteria.hasNumbers) {
-    possibleCharacters = possibleCharacters.concat(Numbers)
-    guaranteedCharacters.push(getRandom(Numbers))
+    possibleCharacters = possibleCharacters.concat(numbers)
+    guaranteedCharacters.push(getRandom(numbers))
 }
 if (passwordCriteria.hasSymbols) {
-    possibleCharacters = possibleCharacters.concat(Symbols)
-    guaranteedCharacters.push(getRandom(Symbols))
+    possibleCharacters = possibleCharacters.concat(symbols)
+    guaranteedCharacters.push(getRandom(symbols))
 }
 //4. display message on page 
 //do same for other criteria, then loop through password criteria then one for possible characters
 //lookup join method
-or (var i = 0; i < options.length; i++) {
-    var possibleCharacter = getRandom(possibleCharacters);
 
-    result.push(possibleCharacter);
-  }
 
-  // Mix in at least one of each guaranteed character in the result
-  for (var i = 0; i < guaranteedCharacters.length; i++) {
-    result[i] = guaranteedCharacters[i];
-  }
+ for (var i = 0; i < passwordCriteria.length; i++) {
+       var possibleCharacter = getRandom(possibleCharacters);
+
+       result.push(possibleCharacter);
+}
+
+
+   // Mix in at least one of each guaranteed character in the result
+   for (var i = 0; i < guaranteedCharacters.length; i++) {
+    console.log(guaranteedCharacters.length)
+     result[i] = guaranteedCharacters[i];
+}
+console.log(result)
+console.log(guaranteedCharacters[i])
+return result.join("")
 
 
 
